813843affcafc7edf17ac66addd6b7a5880620fb
“[firstname <[gmugsk@gmail.com]>
2024-06-19 22:18:27 +0500

    26 Assignment

---
+++ main.js
@@ -0,0 +1,17 @@
+"use strict";
+//26: Alien Colors #2: Choose a color for an alien, then write an if-else chain.
+Object.defineProperty(exports, "__esModule", { value: true });
+let alien_color = "green";
+if (alien_color == "green") {
+    console.log("You just earned 5 points for shooting the alien!");
+}
+else {
+    console.log("You just earned 10 points.");
+}
+alien_color = "yellow";
+if (alien_color == "green") {
+    console.log("You just earned 5 points for shooting the alien!");
+}
+else {
+    console.log("You just earned 10 points.");
+}
